6bc5c7c708d8faa5a3857c90bf78ad4b5b11de5a
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2022-05-15 04:17:53 +0200

    Add active state and aria-expanded handling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,53 @@
 'use strict'
 
+const PRIMARY_NAV_ID = 'primary-nav';
+const PRIMARY_NAV_MENU_ID = 'primary-nav-menu';
 const MOBILE_NAV_BUTTON_ID = 'mobile-nav-button';
-const mobileNavButton = document.getElementById(MOBILE_NAV_BUTTON_ID);
+const ELEMENT_IDS = [PRIMARY_NAV_ID, PRIMARY_NAV_MENU_ID, MOBILE_NAV_BUTTON_ID];
+const ARIA_EXPANDED = 'aria-expanded';
+const elements = getElements();
 
-function toggleClassActive() {
-    this.classList.toggle('active');
+function getParsedElementId(elementId) {
+    return elementId.split('-').map((word, idx) => {
+        if (!idx) return word;
+
+        return `${word[0].toUpperCase()}${word.slice(1)}`;
+    }).join('');
 }
 
-mobileNavButton.addEventListener('click', toggleClassActive);
+function getElement(elementId) {
+    return document.getElementById(elementId);
+}
 
+function getElements() {
+    return ELEMENT_IDS.reduce((pageElements, elementId) => {
+        const parsedElementId = getParsedElementId(elementId);
+
+        pageElements[parsedElementId] = getElement(elementId);
+
+        return pageElements;
+    }, {});
+}
+
+function toggleClassActive(element) {
+    element.classList.toggle('active');
+}
+
+function toggleAriaExpanded(element) {
+    if (element.getAttribute(ARIA_EXPANDED)) {
+        element.setAttribute(ARIA_EXPANDED, 'false');
+    } else {
+        console.log('x');
+        element.setAttribute(ARIA_EXPANDED, 'true'); 
+    }   
+}
+
+function handleMobileNav() {
+    const elements = getElements();
+    toggleClassActive(elements.primaryNav);
+    toggleClassActive(elements.primaryNavMenu);
+    toggleClassActive(elements.mobileNavButton);
+    toggleAriaExpanded(elements.mobileNavButton);
+}
+
+getElement(MOBILE_NAV_BUTTON_ID).addEventListener('click', handleMobileNav);
