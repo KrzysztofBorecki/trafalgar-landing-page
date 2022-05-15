bbfd406d4726ae2bc7a16dc6ecf216a26230edba
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2022-05-15 22:20:09 +0200

    Add toggleScrollLock()

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
 'use strict'
 
+const PAGE_ID = "page";
 const PRIMARY_NAV_ID = 'primary-nav';
 const PRIMARY_NAV_MENU_ID = 'primary-nav-menu';
 const MOBILE_NAV_BUTTON_ID = 'mobile-nav-button';
-const ELEMENT_IDS = [PRIMARY_NAV_ID, PRIMARY_NAV_MENU_ID, MOBILE_NAV_BUTTON_ID];
+const ELEMENT_IDS = [PAGE_ID, PRIMARY_NAV_ID, PRIMARY_NAV_MENU_ID, MOBILE_NAV_BUTTON_ID];
 const ARIA_EXPANDED = 'aria-expanded';
 const elements = getElements();
 
@@ -29,6 +30,11 @@ function getElements() {
     }, {});
 }
 
+
+function toggleScrollLock(element) {
+    element.classList.toggle('scroll-locked');
+}
+
 function toggleClassActive(element) {
     element.classList.toggle('active');
 }
@@ -37,13 +43,13 @@ function toggleAriaExpanded(element) {
     if (element.getAttribute(ARIA_EXPANDED)) {
         element.setAttribute(ARIA_EXPANDED, 'false');
     } else {
-        console.log('x');
         element.setAttribute(ARIA_EXPANDED, 'true'); 
     }   
 }
 
 function handleMobileNav() {
     const elements = getElements();
+    toggleScrollLock(elements.page);
     toggleClassActive(elements.primaryNav);
     toggleClassActive(elements.primaryNavMenu);
     toggleClassActive(elements.mobileNavButton);
