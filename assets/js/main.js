8108ff171de5f96e78b0ac84984c5bde471455e2
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2022-05-16 00:53:35 +0200

    Create branch dev; Add index.html (#1)
    * Create branch dev; Add index.html
    
    * Add normalize.css
    
    * Add page favicon
    
    * Add variables
    
    * Add _variables.scss file
    
    * Add CSS stylesheets and google fonts
    
    * Add style.css and style.css.map
    
    * Clear the code
    
    * Clear the code
    
    * Add page structure
    
    * Add .trafalgar on <html>
    
    * Clean the code
    
    * Add .page on <body>
    
    * Add .png files
    
    * Add logo and basic page/body styles
    
    * Add at-use __grid __navbar
    
    * Clean the code
    
    * Add: nav styles; mobile nav btn
    
    * Comment opacity variables
    
    * Fix comment style
    
    * Add _navbar.scss
    
    * Add _grid.scss
    
    * Add main.js
    
    * Update style.css/css.map files
    
    * Remove logo margin top/bottom
    
    * Fix comment format
    
    * Fix comment format
    
    * Update style.css/css.map files
    
    * Add desktop page-grid
    
    * Add page and menu background
    
    * Add page background and position
    
    * Add main, section, footer grid
    
    * Add mobile and desktop nav-bar styles
    
    * Add grid and mobile nav-bar classes
    
    * Add active state and aria-expanded handling
    
    * Update style.css/css.map files
    
    * Add at-use of navbar-animation
    
    * Add _navbar-animation.scss file
    
    * Clean the code
    
    * Rename _body.scss to _base-styles.scss
    
    * Refactor grid to 'for desktop only'
    
    * Add _elements.scss file
    
    * Add base section style
    
    * Add hero btn styles
    
    * Add btn styles
    
    * Add buttons styles
    
    * Add decorative element styles
    
    * Add decor classes; Fix hero btn
    
    * Add missing section-grid__container classes
    
    * Add elements; Rename body to base-styles
    
    * Update style.css/css.map files
    
    * Remove section padding L/R on desktop
    
    * Add _sections.scss file
    
    * Remove page-grid
    
    * Add section-hero styles
    
    * Remove .page-grid* classes
    
    * Refactor hero section grid
    
    * Add section styles
    
    * Fix mobile nav style and animation
    
    * Add mobile scroll lock
    
    * Add toggleScrollLock()
    
    * Add mobile nav animation and scroll lock
    
    * Fix btn z-index
    
    * Change nav bar height from px to CSS variable
    
    * Add --nav-height-desktop CSS variable
    
    * Add __sections.scss and temporal __DISABLE.scss
    
    * Add temporal __DISABLE.scss file
    
    * Add various section styles
    
    * Add center all page content
    
    * Update style.css/css.map files
    
    * Clear the code
    
    * Fix desktop nav-bar animations
    
    * Add _navigation-footer.scss file
    
    * Add _navigation-general.scss file
    
    * Rename .mobile-top-bar; Move .nav-list
    
    * Remove primary-nav-menu .active toggle
    
    * Add at-use __navigation-general/footer
    
    * Update style.css/css.map files
    
    * Add footer styles, rename .mobile-top-bar

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+'use strict'
+
+const PAGE_ID = "page";
+const PRIMARY_NAV_ID = 'primary-nav';
+const MOBILE_NAV_BUTTON_ID = 'mobile-nav-button';
+const ELEMENT_IDS = [PAGE_ID, PRIMARY_NAV_ID, MOBILE_NAV_BUTTON_ID];
+const ARIA_EXPANDED = 'aria-expanded';
+const elements = getElements();
+
+function getParsedElementId(elementId) {
+    return elementId.split('-').map((word, idx) => {
+        if (!idx) return word;
+
+        return `${word[0].toUpperCase()}${word.slice(1)}`;
+    }).join('');
+}
+
+function getElement(elementId) {
+    return document.getElementById(elementId);
+}
+
+function getElements() {
+    return ELEMENT_IDS.reduce((pageElements, elementId) => {
+        const parsedElementId = getParsedElementId(elementId);
+
+        pageElements[parsedElementId] = getElement(elementId);
+
+        return pageElements;
+    }, {});
+}
+
+
+function toggleScrollLock(element) {
+    element.classList.toggle('scroll-locked');
+}
+
+function toggleClassActive(element) {
+    element.classList.toggle('active');
+}
+
+function toggleAriaExpanded(element) {
+    if (element.getAttribute(ARIA_EXPANDED)) {
+        element.setAttribute(ARIA_EXPANDED, 'false');
+    } else {
+        element.setAttribute(ARIA_EXPANDED, 'true'); 
+    }   
+}
+
+function handleMobileNav() {
+    const elements = getElements();
+    toggleScrollLock(elements.page);
+    toggleClassActive(elements.primaryNav);
+    toggleClassActive(elements.mobileNavButton);
+    toggleAriaExpanded(elements.mobileNavButton);
+}
+
+getElement(MOBILE_NAV_BUTTON_ID).addEventListener('click', handleMobileNav);
